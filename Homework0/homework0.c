07cd6de0ca2dfede1b8918c8c7cee963679cd34a
Scoin0 <tehscoinhub@gmail.com>
2020-01-22 22:14:56 -1000

    Uploaded first homework

---
+++ Homework0/homework0.c
@@ -0,0 +1,22 @@
+/********************************************
+//
+//    NAME:         
+//    HOMEWORK:     Assignment 0
+//    CLASS:        ICS 212
+//    INSTRUCTOR:   
+//    DATE:         Jan 14th, 2020
+//    FILE:         homework0.c
+//    DESCRIPTION:
+//      Create a simple hello world program
+//
+*********************************************/
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    
+    printf("\\Name:\t <NAME>\\\n\\Hello World\\\n\\ICS212 Program Structure\\\n");
+    return 0;
+}
+
+
