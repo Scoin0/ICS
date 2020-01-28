4610de927b640107e7d9a6deba9238e55c02678f
John Lewis <tehscoinhub@gmail.com>
2020-01-28 09:09:43 -1000

    Wrote a shitty skeleton of code.
    This is just so I can send it to my main computer.

--- Homework1/homework1.c
+++ Homework1/homework1.c
@@ -1,4 +1,47 @@
-public int main (void)
+/********************************************
+//
+//    NAME:
+//    HOMEWORK:     Assignment 1
+//    CLASS:        ICS 212
+//    INSTRUCTOR:
+//    DATE:         Jan 27th, 2020
+//    FILE:         homework1.c
+//    DESCRIPTION:
+//
+//
+*********************************************/
+
+#include <stdio.h>
+
+public int main (int argc, char *argv[])
 {
-     
+    prinf("Something something, inches to centimeters.");
+    user_interface();
+    print_table();
+    return 0;
+}
+
+public int user_interface ()
+{
+    int scannedInteger;
+
+
+    printf("Please enter an integer: " + "\n");
+    scanf("%d", &scannedInteger);
+    if (number < 0) {
+        //error out
+    }
+    return scannedInteger;
+}
+
+public double convert (double inches)
+{
+    return inches * 2.54;
+}
+
+public void print_table(int rows)
+{
+    for (int i = 0; i <= rows; i++) {
+
+    }
 }
