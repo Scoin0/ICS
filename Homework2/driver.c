73a0d77c24e2e2de91635848c7f962e1af792249
John Lewis <HobitS282@yahoo.com>
2020-02-04 05:23:39 -1000

    Update driver.c

--- Homework2/driver.c
+++ Homework2/driver.c
@@ -1,9 +1,9 @@
 /***************************************************************************
 //
-//    NAME:         John Lewis
+//    NAME:        
 //    HOMEWORK:     Assignment 2b
 //    CLASS:        ICS 212
-//    INSTRUCTOR:   Ryan Narayan
+//    INSTRUCTOR:   
 //    DATE:         Feb 4th, 2020
 //    FILE:         driver.c
 //    DESCRIPTION:  This is the driver used to test halfstring.c
